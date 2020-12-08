db9391a7a42fedd8dc8885d546b6a4838066f4f0
Kanishk Sharda <kanisk78965@gmail.com>
2020-12-08 19:57:01 +0530

    decrement btn added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const App=()=>{
     const [count,setCount]=useState(0);
     const Inc=()=>{
     setCount(count + 1);};
+    const Dec=()=>{
+        setCount(count - 1);};
     // for time
     let time=new Date().toLocaleTimeString();
     const [ctime,setCtime]=useState(time);
@@ -17,16 +19,27 @@ const App=()=>{
     
     // for background change
     const purple="#8e44ad";
+    const aqua="#00ffff";
     const[bg,setBg]=useState(purple);
-    const[name,setName]=useState("Click Me");
+    const[name,setName]=useState("Click to increase");
+    const[naame,setNaame]=useState("Click to decrease");
     const Bgchange=()=>{
         let nebg='#34495e';
         setBg(nebg);
         setName('One more time😉');
     }
+    const Bggchange=()=>{
+        let ebg='#00ff00';
+        setBg(ebg);
+        setNaame('Ooooo😮');
+    }
     const Bgback=()=>{
         setBg(purple);
         setName('yeah like that😛');
+    }
+    const Bggback=()=>{
+        setBg(aqua);
+        setNaame('my eyes my eyes😫');
     }
 
     return(
@@ -34,7 +47,8 @@ const App=()=>{
         <div style={{backgroundColor:bg}}>
             <h1>{count}</h1>
             <h2>{ctime}</h2>
-            <button  onClick={ function(event){ Inc(); Bgchange()}} onDoubleClick={Bgback}>{name}</button>
+            <button  onClick={ function(event){ Inc(); Bgchange()}} onDoubleClick={Bgback}>{name}</button><br/>
+            <button  onClick={ function(event){ Dec(); Bggchange()}} onDoubleClick={Bggback}>{naame}</button>
         </div>    
         </>
     );
